8849e73fed28de0d815669672d393a437c8ab703
Willian <willianfidelis.duo@gmail.com>
2019-09-06 16:58:50 -0300

    basic functions

---
+++ function/exemplo-01.php
@@ -0,0 +1,15 @@
+<?php
+
+function ola(){
+    return "Hello World!!";
+}
+
+echo ola();
+$frase = ola();
+
+echo "<br>";
+
+echo strlen($frase);
+
+
+?>
---
+++ function/exemplo-02.php
@@ -0,0 +1,9 @@
+<?php
+function salario(){
+    return 946.00;
+}
+
+echo "José receber 3 salários: " . (salario()*3);
+
+
+?>
