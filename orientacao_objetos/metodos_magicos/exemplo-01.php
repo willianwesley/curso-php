974df412c9cce91fbbbb058603eb6499931e7cd4
willianwesley <willianfidelis.duo@gmail.com>
2019-10-14 21:52:13 -0300

    metodos

---
+++ orientacao_objetos/metodos_magicos/exemplo-01.php
@@ -0,0 +1,26 @@
+<?php
+
+class Endereco {
+    private $logradouro;
+    private $numero;
+    private $cidade;
+
+    public function __construct($a, $b, $c){
+        $this->logradouro = $a;
+        $this->numero = $b;
+        $this->cidade = $c;
+    }
+
+    public function __destruct(){
+        var_dump("DESTRUIR");
+    }
+
+    public function __toString(){
+        return $this->logradouro.",". $this->numero. " - " . $this->cidade;
+    }
+}
+
+$meuEndereco = new Endereco("Rua Ariovaldo Kato", "77", "Curitiba");
+
+echo $meuEndereco;
+?>
